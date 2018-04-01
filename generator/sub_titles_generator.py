24f7e4de2eb05086d03099da037824b641e7fd60
Yacin92 <yacincheffai@yahoo.com>
2018-04-01 21:41:49 +0100

    audio to text

--- generator/sub_titles_generator.py
+++ generator/sub_titles_generator.py
@@ -1,21 +1,28 @@
 import speech_recognition as sr
  
-AUDIO_FILE = ("/home/Yacin/Downloads/speech.wav")
- 
-# use the audio file as the audio source
- 
-r = sr.Recognizer()
- 
-with sr.AudioFile(AUDIO_FILE) as source:
-    #reads the audio file. Here we use record instead of
-    #listen
-    audio = r.record(source)  
- 
-try:
-    print("The audio file contains: " + r.recognize_google(audio))
- 
-except sr.UnknownValueError:
-    print("Google Speech Recognition could not understand audio")
- 
-except sr.RequestError as e:
-    print("Could not request results from Google Speech {0}".format(e))
+
+def audio_to_text():
+
+    result = ''
+    AUDIO_FILE = ("/home/Yacin/Downloads/speech.wav")
+    
+    # use the audio file as the audio source
+    
+    r = sr.Recognizer()
+    
+    with sr.AudioFile(AUDIO_FILE) as source:
+        #reads the audio file. Here we use record instead of
+        #listen
+        audio = r.record(source)  
+    
+    try:
+        result = "The audio file contains: " + r.recognize_google(audio)
+    
+    except sr.UnknownValueError:
+        result = "Google Speech Recognition could not understand audio"
+    
+    except sr.RequestError as e:
+        result = "Could not request results from Google Speech {0}".format(e)
+    return result
+
+print(audio_to_text())
