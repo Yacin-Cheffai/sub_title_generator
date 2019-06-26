29a226af56562e3377226f2e9d3ac4dd88456c96
Yacin-Cheffai <yacincheffai@yahoo.com>
2019-06-26 17:18:25 +0100

    [004][IMP] error and success messages

--- generator/views.py
+++ generator/views.py
@@ -5,52 +5,56 @@ import os
 from .sub_titles_generator import audio_to_text, force_align
 from django.http import HttpResponse, Http404
 import subprocess
+from django.contrib import messages
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 def upload(request):
-    
-    if request.method == 'POST' and request.FILES['myfile']:
-        myfile = request.FILES['myfile']
+    try:
+        if request.method == 'POST' and request.FILES['myfile']:
+            myfile = request.FILES['myfile']
 
-        if myfile.name[-4:] ==  '.mp3':
-            # convert mp3 to wav
-            myfile.name = 'uploaded_file.mp3'
-            fs = FileSystemStorage()
-            filename = fs.save(myfile.name, myfile)
-            uploaded_file_url = fs.url(filename)
-            file_path = BASE_DIR + uploaded_file_url
-            path_to_wav_file = file_path[:-4] + '.wav'
-            subprocess.call(['ffmpeg', '-i', file_path,  path_to_wav_file])
-            audio_to_text(path_to_wav_file)
-            force_align(path_to_wav_file, BASE_DIR + '/results.txt', BASE_DIR)
-            context = {
-                'uploaded_file_url': uploaded_file_url,
-                'uploaded_successfully' : True
-            }
-            return render(request, 'generator/upload.html', context)
+            if myfile.name[-4:] ==  '.mp3':
+                # convert mp3 to wav
+                myfile.name = 'uploaded_file.mp3'
+                fs = FileSystemStorage()
+                filename = fs.save(myfile.name, myfile)
+                uploaded_file_url = fs.url(filename)
+                file_path = BASE_DIR + uploaded_file_url
+                path_to_wav_file = file_path[:-4] + '.wav'
+                subprocess.call(['ffmpeg', '-i', file_path,  path_to_wav_file])
+                audio_to_text(path_to_wav_file)
+                force_align(path_to_wav_file, BASE_DIR + '/results.txt', BASE_DIR)
+                context = {
+                    'uploaded_file_url': uploaded_file_url,
+                    'uploaded_successfully' : True
+                }
+                messages.success(request, 'SRT file successfully generated, your file won\'t be stored in our systems')
+                return render(request, 'generator/upload.html', context)
 
-        if myfile.name[-4:] ==  '.wav':
-            myfile.name = 'uploaded_file.wav'
-            fs = FileSystemStorage()
-            filename = fs.save(myfile.name, myfile)
-            uploaded_file_url = fs.url(filename)
-            file_path = BASE_DIR + uploaded_file_url
-            audio_to_text(file_path)
-            force_align(file_path, BASE_DIR + '/results.txt', BASE_DIR)
-            context = {
-                'uploaded_file_url': uploaded_file_url,
-                'uploaded_successfully' : True
-            }
-            return render(request, 'generator/upload.html', context)
+            if myfile.name[-4:] ==  '.wav':
+                myfile.name = 'uploaded_file.wav'
+                fs = FileSystemStorage()
+                filename = fs.save(myfile.name, myfile)
+                uploaded_file_url = fs.url(filename)
+                file_path = BASE_DIR + uploaded_file_url
+                audio_to_text(file_path)
+                force_align(file_path, BASE_DIR + '/results.txt', BASE_DIR)
+                context = {
+                    'uploaded_file_url': uploaded_file_url,
+                    'uploaded_successfully' : True
+                }
+                return render(request, 'generator/upload.html', context)
 
-        if myfile.name[-4:] is not   '.mp3' and myfile.name[-4:] is not  '.wav':
-            return HttpResponse( 'unsupported file format')
+            if myfile.name[-4:] is not   '.mp3' and myfile.name[-4:] is not  '.wav':
+                return HttpResponse( 'unsupported file format')
 
-    return render(request, 'generator/upload.html')
-
+        return render(request, 'generator/upload.html')
+    except:
+        messages.warning(request, 'make sure you selected the right file')
+        return render(request, 'generator/upload.html')  
 
 def download(request):
     
